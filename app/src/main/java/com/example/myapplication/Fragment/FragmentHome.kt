2d08fa64a72d5c8af0d80e72221f36199c0aae17
billythefighter <BillyBeHeroes123*>
2020-04-13 06:35:41 +0700

    home half done(-adapter)

---
+++ app/src/main/java/com/example/myapplication/Fragment/FragmentHome.kt
@@ -0,0 +1,26 @@
+package com.example.myapplication.Fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.myapplication.R
+
+class FragmentHome : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    companion object {
+        fun newInstance(): FragmentHome{
+            val fragment = FragmentHome()
+            val args = Bundle()
+            fragment.arguments = args
+            return fragment
+        }
+    }
+}
